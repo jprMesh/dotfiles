0ec00104bd071567d40d63dd9dc1c72c0aa7ceec
jprMesh <jprmesh@gmail.com>
2021-03-06 21:58:47 -0800

    updated gitconfig

--- gitconfig.sh
+++ gitconfig.sh
@@ -1,10 +1,13 @@
-#git config --global user.name jprMesh
-#git config --global user.email ------
+git config --global user.name jprMesh
+git config --global user.email jprmesh@gmail.com
 
 git config --global core.editor vi
 git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
+git config --global alias.ch cherry-pick
+git config --global alias.rb rebase
 git config --global alias.lg 'log --decorate --graph'
+git config --global alias.logall 'log --oneline --decorate --graph --all'
 git config --global alias.unstage 'reset HEAD --'
